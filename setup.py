f9a2b701c36536f123f576ef2a3e4c6ad1a7d121
Marseel-E <marseeleeso@gmail.com>
2022-04-15 19:04:38 -0400

    renamed package

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ with open("README.md", 'r', encoding="utf-8") as fh:
 
 
 setuptools.setup(
-    name="bleep",
+    name="bleep-it",
     version="0.1.0",
     author="Marseel Eeso",
     author_email="marseeleeso@gmail.com",
     description="Text filtering tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Marseel-E/bleep",
+    url="https://github.com/Marseel-E/bleep-it",
     project_urls={
-        "Bug Tracker": "https://github.com/Marseel-E/bleep/issues"
+        "Bug Tracker": "https://github.com/Marseel-E/bleep-it/issues"
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "bleep"},
-    packages=setuptools.find_packages(where="bleep"),
+    package_dir={"": "bleep-it"},
+    packages=setuptools.find_packages(where="bleep-it"),
     python_requires=">=3.9",
 )
