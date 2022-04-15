1cdaf98cd5e095427c6c6eaf464bf40d4e142869
Marseel-E <marseeleeso@gmail.com>
2022-04-15 19:15:51 -0400

    fixed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r', encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bleep-it",
-    version="0.1.0",
+    version="0.1.1",
     author="Marseel Eeso",
     author_email="marseeleeso@gmail.com",
     description="Text filtering tool",
